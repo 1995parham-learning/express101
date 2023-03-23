36f85ac4eb3e0aa09e0eb6e560b0e3c0fed5c43b
Parham Alvani <parham.alvani@gmail.com>
2023-03-24 01:18:04 +0330

    feat: setup cluster based on number of available cores

--- index.ts
+++ index.ts
@@ -1,8 +1,28 @@
 import { config } from "./src/config/config";
 import { app } from "./src/app/app";
+import { availableParallelism } from "node:os";
+import cluster from "node:cluster";
+import process from "node:process";
 
-app.listen(config.port, () => {
-  console.log(
-    `⚡️[server]: Server is running at http://localhost:${config.port}`
-  );
-});
+const numCPUs = availableParallelism();
+
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  app.listen(config.port, () => {
+    console.log(
+      `⚡️[server]: Server is running at http://localhost:${config.port}`
+    );
+  });
+
+  console.log(`Worker ${process.pid} started`);
+}
